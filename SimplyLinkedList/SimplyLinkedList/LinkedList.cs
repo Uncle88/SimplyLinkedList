eaecbf797151e5e9e06a02f0aa6d43e70e977312
Uncle88 <andreyproskura88@gmail.com>
2017-08-22 17:28:38 +0300

    add file LinkedList

---
+++ SimplyLinkedList/SimplyLinkedList/LinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SimplyLinkedList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public LinkedList()
+        {
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
